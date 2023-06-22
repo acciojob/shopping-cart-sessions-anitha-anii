97ce3b4e8f7b9942f8987bc108bf3c3f8781f291
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-22 13:13:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,42 +1,13 @@
 // This is the boilerplate code given for you
 // You can modify this code
 // Product data
-// const products = [
-//   { id: 1, name: "Product 1", price: 10 },
-//   { id: 2, name: "Product 2", price: 20 },
-//   { id: 3, name: "Product 3", price: 30 },
-//   { id: 4, name: "Product 4", price: 40 },
-//   { id: 5, name: "Product 5", price: 50 },
-// ];
-
 const products = [
-  {
-    "id": 1,
-    "name": "Product 1",
-    "price": 10
-  },
-  {
-    "id": 2,
-    "name": "Product 2",
-    "price": 20
-  },
-  {
-    "id": 3,
-    "name": "Product 3",
-    "price": 30
-  }
-	{
-    "id": 4,
-    "name": "Product 4",
-    "price": 40
-  }
-		{
-    "id": 5,
-    "name": "Product 5",
-    "price": 50
-  }
+  { id: 1, name: "Product 1", price: 10 },
+  { id: 2, name: "Product 2", price: 20 },
+  { id: 3, name: "Product 3", price: 30 },
+  { id: 4, name: "Product 4", price: 40 },
+  { id: 5, name: "Product 5", price: 50 },
 ];
-
 
 // DOM elements
 const productList = document.getElementById("product-list");
@@ -69,7 +40,7 @@ function addToCart(productId) {
  const product = products.find((p) => p.id === productId);
   if (product) {
     const cart = JSON.parse(sessionStorage.getItem("cart")) || [];
-    cart.push(product); // Allow duplicates by always pushing the product
+    cart.push(product);
     sessionStorage.setItem("cart", JSON.stringify(cart));
     renderCart();
   }
