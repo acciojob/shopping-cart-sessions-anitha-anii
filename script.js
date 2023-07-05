55bffc1a580832d4e7f12009f39d55c515223b01
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-07-05 16:05:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,39 +1,59 @@
- function updateCartUI() {
-      var cartItemsElement = document.getElementById('product-list');
-      var cartItems = JSON.parse(sessionStorage.getItem('cart')) || [];
+// Function to add a product to the cart
+    function addToCart(event) {
+      const product = event.currentTarget;
+      const productId = product.dataset.id;
+      const productName = product.dataset.name;
+      const productPrice = product.dataset.price;
 
-      // Clear the cart items
-      cartItemsElement.innerHTML = '';
+      const cart = getCartData();
 
-      // Add the cart items to the UI
-      cartItems.forEach(function (item) {
-        var li = document.createElement('li');
-        li.textContent = item;
-        cartItemsElement.appendChild(li);
+      // Check if the product is already in the cart
+      const existingProduct = cart.find(item => item.id === productId);
+      if (existingProduct) {
+        existingProduct.quantity++;
+      } else {
+        cart.push({ id: productId, name: productName, price: productPrice, quantity: 1 });
+      }
+
+      // Save the updated cart data
+      saveCartData(cart);
+
+      // Refresh the cart display
+      displayCartItems();
+    }
+
+    // Function to get the cart data from session storage
+    function getCartData() {
+      const cartData = sessionStorage.getItem('cart');
+      return cartData ? JSON.parse(cartData) : [];
+    }
+
+    // Function to save the cart data to session storage
+    function saveCartData(cart) {
+      sessionStorage.setItem('cart', JSON.stringify(cart));
+    }
+
+    // Function to display the cart items
+    function displayCartItems() {
+      const cartItems = document.getElementById('cartItems');
+      cartItems.innerHTML = '';
+
+      const cart = getCartData();
+
+      cart.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = `${item.name} - $${item.price} x ${item.quantity}`;
+        cartItems.appendChild(li);
       });
     }
 
-    // Function to add a product to the cart
-    function addToCart(event) {
-      var product = event.target.parentNode.textContent.trim();
-      var cartItems = JSON.parse(sessionStorage.getItem('cart')) || [];
-
-      // Add the product to the cart
-      cartItems.push(product);
-
-      // Store the updated cart in session storage
-      sessionStorage.setItem('cart', JSON.stringify(cartItems));
-
-      // Update the cart UI
-      updateCartUI();
+    // Add event listeners to product list items
+    const productList = document.getElementById('productList');
+    const productItems = productList.getElementsByTagName('li');
+    for (let i = 0; i < productItems.length; i++) {
+      productItems[i].addEventListener('click', addToCart);
     }
 
-    // Add event listeners to the add-to-cart buttons
-    var addToCartButtons = document.querySelectorAll('.add-to-cart');
-    addToCartButtons.forEach(function (button) {
-      button.addEventListener('click', addToCart);
-    });
-
-    // Initialize the cart UI on page load
-    updateCartUI();
+    // Display the cart items on page load
+    displayCartItems();
 
